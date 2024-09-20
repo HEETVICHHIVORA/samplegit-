be9d0f0bd78678c9bc64b0cab79ca7880a558431
heetcodes <heetvic@gmail.com>
2024-09-20 10:00:55 +0530

    a1

---
+++ a1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std; 
+int main (){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    int n ; 
+    cin>>n ;
+    cout<<n<<endl;
+    return 0 ;
+}
