fbbe5c42e13ebe763cff5e35daa69cfd5a5e2523
heetcodes <heetvic@gmail.com>
2024-09-20 09:53:39 +0530

    commit in b

--- b.cpp
+++ b.cpp
@@ -5,5 +5,6 @@ int main (){
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     cout<<"cchnage in the b brnahc "<<endl;
+    cout<<"hello workd"<<endl;
     return 0 ;
 }
