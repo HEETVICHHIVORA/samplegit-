f93abfcd6229bd934ddc405935847582121064d1
heetcodes <heetvic@gmail.com>
2024-09-20 09:58:01 +0530

    a.cpp

--- a.cpp
+++ a.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
-
+int even(){
+    return 0 ;
+}
 using namespace std; 
 int main (){
     freopen("input.txt","r",stdin);
@@ -7,5 +9,6 @@ int main (){
     int n ; 
     cin>>n ;
     cout<<n<<endl;
+
     return 0 ;
 }
