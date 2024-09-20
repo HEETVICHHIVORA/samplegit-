a866fe7899a74711e94df396e89e28e963b044e0
heetcodes <heetvic@gmail.com>
2024-09-20 09:50:46 +0530

    dd

---
+++ b.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std; 
+int main (){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    cout<<"cchnage in the b brnahc "<<endl;
+
+    return 0 ;
+}
